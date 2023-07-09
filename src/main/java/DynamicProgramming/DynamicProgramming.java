8c95ea4368b52324284c67b3fa2a45829a3427b9
Swapnil Shah <swapnilshah5889@gmail.com>
2023-07-09 11:13:26 -0400

    DP: Implemented fibonacci using bottom up iterative approach

--- src/main/java/DynamicProgramming/DynamicProgramming.java
+++ src/main/java/DynamicProgramming/DynamicProgramming.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class DynamicProgramming {
 
     // Find Nth Fibonacci Number
-    public static int findFib(int n, int[] arr) {
+    public static int findFibTopDown(int n, int[] arr) {
         if(n == 1 || n == 2) {
             return arr[n];
         }
@@ -14,19 +14,42 @@ public class DynamicProgramming {
             return arr[n];
         }
 
-        int ans = findFib(n-1, arr) + findFib(n-2, arr);
+        int ans = findFibTopDown(n-1, arr) + findFibTopDown(n-2, arr);
         arr[n] = ans;
         return ans;
+    }
+
+    public static int findFibBottomUp(int n) {
+
+        if(n == 1 || n == 2) {
+            return 1;
+        }
+
+        int fibPrev = 1;
+        int fibPrevPrev = 1;
+        int index = 3;
+        int fib = fibPrevPrev;
+        while(index<=n) {
+            fib = fibPrev + fibPrevPrev;
+            fibPrevPrev = fibPrev;
+            fibPrev = fib;
+            index++;
+        }
+
+        return fib;
+
     }
 
     public static void main(String[] args) {
 
         // Find Nth Fibonacci number
-        int n = 8;
+        int n = 1;
         int arr[] = new int[n+1];
         Arrays.fill(arr,-1);
         arr[1] = 1;
-        arr[2] = 1;
-        System.out.println("Fibonacci "+n+": "+findFib(n, arr));
+        if(arr.length>2)
+            arr[2] = 1;
+        System.out.println("Fibonacci(Recursive) "+n+": "+findFibTopDown(n, arr));
+        System.out.println("Fibonacci(Iterative) "+n+": "+findFibBottomUp(n));
     }
 }
